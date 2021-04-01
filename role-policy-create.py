e2d71bcd74c7100055f3df096feb1299a53777d4
YGVPRASAD <55577916+YGVPRASAD@users.noreply.github.com>
2021-04-01 15:30:41 +0530

    Update role-policy-create.py

--- role-policy-create.py
+++ role-policy-create.py
@@ -23,18 +23,21 @@ for opt, arg in opts:
         #print(policy_name)
         for ppart in policy_name.split(","):
             print(ppart)
-            f = open(ppart+'.json', 'r')
-            pvalue = f.read()
-            #print(pvalue)
-            policy_document = json.loads(json.dumps(pvalue))
-            #print(assume_policy_document)
-            client = boto3.client('iam')
-            create_policy_response = client.create_policy(
-                PolicyName = ppart,
-                PolicyDocument = policy_document
-            )
-            #print(create_policy_response)
-            
+            try:
+                f = open(ppart+'.json', 'r')
+                pvalue = f.read()
+                #print(pvalue)
+                policy_document = json.loads(json.dumps(pvalue))
+                #print(assume_policy_document)
+                client = boto3.client('iam')
+                create_policy_response = client.create_policy(
+                    PolicyName = ppart,
+                    PolicyDocument = policy_document
+                )
+                #print(create_policy_response)
+            except ClientError as error:
+                if error.response['Error']['Code'] == 'EntityAlreadyExists':
+                    print('Policy already exists... hence exiting from here')
             iam = boto3.client('iam')
             response = iam.list_policies(
                 Scope = 'All' # 'AWS'|'Local'|'All'
@@ -52,18 +55,21 @@ for opt, arg in opts:
         i = 0
         for rpart in role_name.split(","):
             print(rpart)
-            f = open(rpart+'.json', 'r')
-            rvalue = f.read()
-            #print(rvalue)
-            assume_role_document = json.loads(json.dumps(rvalue))
-            #print(assume_role_document)
-            client = boto3.client('iam')
-            create_role_response = client.create_role(
-                RoleName = rpart,
-                AssumeRolePolicyDocument = assume_role_document
-            )
+            try:
+                f = open(rpart+'.json', 'r')
+                rvalue = f.read()
+                #print(rvalue)
+                assume_role_document = json.loads(json.dumps(rvalue))
+                #print(assume_role_document)
+                client = boto3.client('iam')
+                create_role_response = client.create_role(
+                    RoleName = rpart,
+                    AssumeRolePolicyDocument = assume_role_document
+                )
 
-            attach_response = client.attach_role_policy(
-                RoleName=rpart, PolicyArn=l[i])
-            i = i + 1
-            
+                attach_response = client.attach_role_policy(
+                    RoleName=rpart, PolicyArn=l[i])
+                i = i + 1
+            except ClientError as error:
+                if error.response['Error']['Code'] == 'EntityAlreadyExists':
+                    print('Role already exists... hence exiting from here')
